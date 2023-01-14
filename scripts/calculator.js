cc2462ea04693107ceffde9cb8eb4f2b556a475a
jake-abed <jakeabed@gmail.com>
2023-01-13 18:04:10 -0800

    Add operator functions & script tag

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,10 +2,38 @@
 
 //Initialize global object
 const calculator = {
-    operand1: null,
-    operand2: null,
+    currentOperand: null,
+    lastOperand: null,
     operator: null,
-    result: null
+    result: null,
+    displayValue: null
 }
 
 //Begin further planning
+
+//Basic math functions for add, subtract, multiply, divide.
+
+const add = (x, y) => Number(x) + Number(y);
+const subtract = (x, y) => Number(x) - Number(y);
+const multiply = (x, y) => Number(x) * Number(y);
+const divide = (x, y) => Number(x) / Number(y);
+
+//Operate takes operator and two values and runs the equation.
+const operate = (operator, x, y) => {
+    switch (operator) {
+        case '+':
+            return add(x, y);
+            break;
+        case '-':
+            return subtract(x, y);
+            break;
+        case '*':
+            return multiply(x, y);
+            break;
+        case '/':
+            return divide(x, y);
+            break;
+    }
+}
+
+//Update display on button press
