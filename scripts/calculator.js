59aa923b61a2888a049a9ddf9532b62a19a413ab
jake-abed <jakeabed@gmail.com>
2023-01-13 21:08:51 -0800

    Declare button consts, display helpers, & more
    -Add button constants for all user interactable elements on calc.
    -Add addToDisplay and clearDisplay helpers to modify the screen.
    -Added CSS to make the screen look a little better and wrap with
    excess characters.
    -Overhaul classes on all button divs on the main calculator to
    be better in-line with naming conventions.
    
    Wrapping up for the night. That was a good little session and the
    code is looking clean. I have a good plan in place to wrap it up
    too :)

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,18 +1,24 @@
 'use strict';
 
+//declare DOM Buttons & Screen
+const calcScreen = document.querySelector('.screen-text'),
+	numberButtons = document.querySelectorAll('.number'),
+	clearButton = document.querySelector('.clear.button'),
+	signButton = document.querySelector('.sign'),
+	operatorButtons = document.querySelectorAll('.operator'),
+	pointButton = document.querySelector('.point'),
+	equalButton = document.querySelector('.equal');
+
 //Initialize global object
 const calculator = {
-    currentOperand: null,
-    lastOperand: null,
-    operator: null,
-    result: null,
-    displayValue: null
+	currentOperand: null,
+	lastOperand: null,
+	operator: null,
+	result: null,
+	displayValue: ''
 }
 
-//Begin further planning
-
 //Basic math functions for add, subtract, multiply, divide.
-
 const add = (x, y) => Number(x) + Number(y);
 const subtract = (x, y) => Number(x) - Number(y);
 const multiply = (x, y) => Number(x) * Number(y);
@@ -20,20 +26,36 @@ const divide = (x, y) => Number(x) / Number(y);
 
 //Operate takes operator and two values and runs the equation.
 const operate = (operator, x, y) => {
-    switch (operator) {
-        case '+':
-            return add(x, y);
-            break;
-        case '-':
-            return subtract(x, y);
-            break;
-        case '*':
-            return multiply(x, y);
-            break;
-        case '/':
-            return divide(x, y);
-            break;
-    }
+		switch (operator) {
+		case '+':
+			return add(x, y);
+			break;
+		case '-':
+			return subtract(x, y);
+			break;
+		case '*':
+			return multiply(x, y);
+			break;
+		case '/':
+			return divide(x, y);
+			break;
+	}
+}
+
+//helper function to update the display with new value
+const addToDisplay = (newDisplayValue) => {
+	calculator.displayValue += newDisplayValue;
+	return calcScreen.innerText = calculator.displayValue;
+}
+
+//Helper function to clear display
+const clearDisplay = () => {
+	calculator.displayValue = '';
+	return calcScreen.innerText = '';
 }
 
 //Update display on button press
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => addToDisplay(button.dataset.val));
+})
